5edfc2d8f33c6edec927788444a7400a3afe1c41
GulyasLali <gulyaslajos2001@gmail.com>
2023-11-09 12:16:15 +0100

    Delete therealcookbook directory

--- therealcookbook/src/main/java/com/example/therealcookbook/Ingredients/IngredientController.java
+++
@@ -1,4 +0,0 @@
-package com.example.therealcookbook.Ingredients;
-
-public class IngredientController {
-}
--- therealcookbook/src/main/java/com/example/therealcookbook/Ingredients/IngredientRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.therealcookbook.Ingredients;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IngredientRepository extends JpaRepository<Ingredient, Integer>{
-}
